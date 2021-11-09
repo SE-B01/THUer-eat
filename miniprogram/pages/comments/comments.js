f1aa2ca08d9d5de5be48f6f6fdc4078472dd6364
qsj <qsj19@mails.tsinghua.edu.cn>
2021-11-09 22:37:34 +0800

    评论页面（草稿）
    未添加样式，js文件不完善

---
+++ miniprogram/pages/comments/comments.js
@@ -0,0 +1,70 @@
+// pages/comments/comments.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        canteen : {name:"桃李园-二层", dish:["辣子鸡","咕佬肉","荷塘小炒","香辣豆皮","水煮肉","地三鲜","叉烧鸡腿"]},
+        starlist : ['yellow', 'yellow', 'gray', 'gray', 'gray'],
+        index:null,
+        imgList: [],
+        paymethod: ['仅校园卡', '校园卡及其它方式', '仅其他方式'],
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
