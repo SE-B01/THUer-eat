1ff1124e542db71c64c879facdfcae06b1f0da94
qsj <qsj19@mails.tsinghua.edu.cn>
2021-11-11 14:59:33 +0800

    merge

--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,66 +1,36 @@
-// pages/map/map.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+   longitude: 113.324520,
+   latitude: 23.099994,
+   markers:[{
+     id: 0,
+     iconPath: "../../images/icon_cur_position.png",
+     latitude: 23.099994,
+     longitude: 113.324520,
+     width: 50,
+     height: 50
+   }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function(){
+    var that = this;
+    wx.getLocation({
+      type: "wgs84",
+      success: function(res){
+        var latitude = res.latitude;
+        var longitude = res.longitude;
+       //console.log(res.latitude);
+        that.setData({
+         latitude: res.latitude,
+         longitude: res.longitude,
+         markers:[{
+           latitude: res.latitude,
+           longitude: res.longitude
+         }]
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  onReady: function(){
 
   }
 })
