e13c2a7d08c3ab78f7e0cfe752051fc48ce81113
qsj <qsj19@mails.tsinghua.edu.cn>
2021-11-11 15:13:46 +0800

    merge

---
+++ docs/colorUI技术分享.md
@@ -0,0 +1,11 @@
+下载源码解压获得`/demo`，复制目录下的 `/colorui` 文件夹到`THUER-EAT/miniprogram`
+
+`app.wxss`中引入关键CSS：
+
+```css
+@import "colorui/main.wxss";
+@import "colorui/icon.wxss";
+```
+
+使用微信开发者工具新建项目，导入源码中的`/demo`文件夹，可在示例项目中查看大部分样式、组件使用方法及效果。
+
--- miniprogram/pages/dishes/dishes.js
+++ miniprogram/pages/dishes/dishes.js
@@ -1,21 +1,15 @@
 Page({
   data: {
-    TabCur: 0,
-    scrollLeft:0,
-    TabName: ["评价","菜品","问大家"],
-    TabNumber: [32, 20, 15],
     cardCur: 0,
     buttonColor: {
       title: '木槿',
        name: 'mauve',
       color: '#9c26b0'
     },
-    canteenInfo: {
-      name: "荷园",
-      price: 25,
-      grade: 3,
-      address: "清华大学内荷花池畔",
-
+    dishesInfo: {
+      name: "红烧肉",
+      price: 10,
+      address: "清华大学内荷花池畔荷园教职工餐厅（二层）",
       time: "11:00-19:00"
     },
     swiperList: [{
@@ -47,26 +41,6 @@ Page({
       type: 'image',
       url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
     }],
-    ColorList: [{
-      title: '人较少',
-      name: 'red',
-      color: '#e54d42'
-    },
-    {
-      title: '口味不错',
-      name: 'orange',
-      color: '#f37b1d'
-    },
-    {
-      title: '食材新鲜',
-      name: 'yellow',
-      color: '#fbbd08'
-    },
-    {
-      title: '上菜快',
-      name: 'olive',
-      color: '#8dc63f'
-    }]
   },
   onLoad() {
 
@@ -75,12 +49,6 @@ Page({
   cardSwiper(e) {
     this.setData({
       cardCur: e.detail.current
-    })
-  },
-  tabSelect(e) {
-    this.setData({
-      TabCur: e.currentTarget.dataset.id,
-      scrollLeft: (e.currentTarget.dataset.id-1)*60
     })
   }
 })
