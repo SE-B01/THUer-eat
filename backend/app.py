2d5eb907fbd19f76296413e32f7cd152096cd350
ylf <2872444509@qq.com>
2021-11-16 15:20:09 +0800

    前后端交互示例 test.js

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask_cors import *
+from flask import request, jsonify
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    data = [{'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}]
+    return json.dumps(data)
+    #return 'Hello World!'
 
+@app.route('/test')
+def test():
+    #param = request.get_json()
+    param  = request.args.get('test')
+    print(f'param: {param}')
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run()
