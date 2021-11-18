c1aae898c05660120840d9f293f0259ca1700e4a
ylf <2872444509@qq.com>
2021-11-18 15:48:59 +0800

    test.js示例

--- backend/appraise/appraise.py
+++ backend/appraise/appraise.py
@@ -4,6 +4,7 @@ import json
 
 appraise = Blueprint('appraise', __name__)
 
+# 前端 -> 后端
 @appraise.route('/appraise_test', methods=['GET', 'POST'])
 def appraise_example():
     #appraise_ = Appraise.query.first()
@@ -13,3 +14,11 @@ def appraise_example():
     args = request.args.get('test')
     print(args)
     return args, 200
+
+# 后端 -> 前端
+@appraise.route('/appraise_test2', methods=['GET', 'POST'])
+def appraise_example2():
+    appraise_ = Appraise.query.first()
+    appraise_dish_json = '{"appraise": ' + appraise_.dish + '}'
+    print(appraise_dish_json)
+    return json.loads(appraise_dish_json), 200
