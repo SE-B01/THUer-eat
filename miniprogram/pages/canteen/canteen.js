ca501b3a3e92be48886e334bc6a67f8728a0ba5f
qsj <qsj19@mails.tsinghua.edu.cn>
2021-11-20 16:43:36 +0800

    去除餐厅页面前端硬编码

--- miniprogram/pages/canteen/canteen.js
+++ miniprogram/pages/canteen/canteen.js
@@ -3,27 +3,31 @@ Page({
   data: {
     //canteen接受其它页面传参，显示当前的餐厅
     canteen: "",
-
+    apprise: [],
+    starlist: ['gray', 'gray', 'gray', 'gray', 'gray'],
     TabCur: 0,
-    scrollLeft:0,
-    TabName: ["评价","菜品","问大家"],
+    scrollLeft: 0,
+    TabName: ["评价", "菜品", "问大家"],
     TabNumber: [32, 20, 15],
     cardCur: 0,
     buttonColor: {
       title: '木槿',
-       name: 'mauve',
+      name: 'mauve',
       color: '#9c26b0'
     },
     canteenInfo: [],
+    location: "",
     comments: [],
+    cost: 0,
+    apprise_list: [],
     swiperList: [{
       id: 0,
       type: 'image',
       url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
     }, {
       id: 1,
-        type: 'image',
-        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
     }, {
       id: 2,
       type: 'image',
@@ -66,28 +70,27 @@ Page({
       color: '#8dc63f'
     }]
   },
-  onLoad: function(options) {
+  onLoad: function (options) {
     var that = this;
     that.setData({
       canteen: options.canteen
+      // canteen: "桃李园-一层"
     })
-    console.log("当前浏览餐厅:"+that.data.canteen)
-
-
-    db.collection('canteen').get({
-      success: res =>{
-          this.setData({
-            canteenInfo: res.data
-          })
-          //console.log(res.data);
-          //console.log(this);
-      }
-    });
-    db.collection('appraise_canteen').where({
-      "canteen_id": '859059a561909a1505ad6b675e098532'
-    })
-    .get({
-      success: function(res) {
+    wx.request({
+      url: 'http://127.0.0.1:5000/canteen/get',
+      data: {
+        name: this.data.canteen
+        // name: "桃李园-一层"
+      },
+      method: 'GET',
+      success: (res) => {
+        this.setData({
+          location: res.data.location,
+          business_hours: res.data.business_hours,
+          starlist: res.data.starlist,
+          cost: res.data.cost,
+          apprise_list: res.data.ap_list
+        })
         console.log(res.data)
       }
     })
@@ -101,7 +104,7 @@ Page({
   tabSelect(e) {
     this.setData({
       TabCur: e.currentTarget.dataset.id,
-      scrollLeft: (e.currentTarget.dataset.id-1)*60
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
     })
   }
 })
