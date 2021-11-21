bd338af6a6703ee180ce0ad6980acacc29349589
ylf <2872444509@qq.com>
2021-11-21 11:57:27 +0800

    餐厅前端修改

--- miniprogram/pages/canteen/canteen.js
+++ miniprogram/pages/canteen/canteen.js
@@ -24,31 +24,31 @@ Page({
     swiperList: [{
       id: 0,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+      url: '../../images/canteens/听涛1.jpg'
     }, {
       id: 1,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+      url: '../../images/canteens/听涛2.jpg',
     }, {
       id: 2,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+      url: '../../images/canteens/听涛1.jpg'
     }, {
       id: 3,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+      url: '../../images/canteens/听涛2.jpg'
     }, {
       id: 4,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+      url: '../../images/canteens/听涛1.jpg'
     }, {
       id: 5,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+      url: '../../images/canteens/听涛2.jpg'
     }, {
       id: 6,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+      url: '../../images/canteens/听涛1.jpg'
     }],
     ColorList: [{
       title: '人较少',
