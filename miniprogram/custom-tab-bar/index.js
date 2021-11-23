ff5ebd22d57f447dba44b640087eea64ee5e0630
Amadeus <yuxiang-zhang2000@outlook.com>
2021-11-23 15:34:39 +0800

    导航栏颜色样式修改、首页筛选框升级

--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -4,65 +4,65 @@ Component({
   data: {
     selected: 0,
     "color": "#a9b7b7",
-    "selectedColor": "#1296db",
+    "selectedColor": "#7b10a8",
     list_general:[
       {
-        selectedIconPath: "../images/icons/host_on.png",
-        iconPath: "../images/icons/host.png",
+        selectedIconPath: "../images/icons/home_on.png",
+        iconPath: "../images/icons/home.png",
         pagePath: "/pages/home/home",
         text: "首页"
       },
       {
-        selectedIconPath: "../images/icons/feedback_on.png",
-        iconPath: "../images/icons/feedback.png",
+        selectedIconPath: "../images/icons/map_on.png",
+        iconPath: "../images/icons/map.png",
         pagePath: "/pages/map/map",
         text: "地图"
       },
 
       {
-        selectedIconPath: "../images/icons/feedback_on.png",
-        iconPath: "../images/icons/feedback.png",
+        selectedIconPath: "../images/icons/mine_on.png",
+        iconPath: "../images/icons/mine.png",
         pagePath: "/pages/newmine/newmine",
         text: "我的"
       }
     ],
     list_admin: [
       {
-        selectedIconPath: "../images/icons/host_on.png",
-        iconPath: "../images/icons/host.png",
+        selectedIconPath: "../images/icons/home_on.png",
+        iconPath: "../images/icons/home.png",
         pagePath: "/pages/home/home",
         text: "首页"
       },
       {
-        selectedIconPath: "../images/icons/feedback_on.png",
-        iconPath: "../images/icons/feedback.png",
+        selectedIconPath: "../images/icons/map_on.png",
+        iconPath: "../images/icons/map.png",
         pagePath: "/pages/map/map",
         text: "地图"
       },
 
       {
-        selectedIconPath: "../images/icons/feedback_on.png",
-        iconPath: "../images/icons/feedback.png",
+        selectedIconPath: "../images/icons/mine_on.png",
+        iconPath: "../images/icons/mine.png",
         pagePath: "/pages/newmine/newmine",
         text: "我的"
       }],
       list:[
       {
-        selectedIconPath: "../images/icons/host_on.png",
-        iconPath: "../images/icons/host.png",
+        selectedIconPath: "../images/icons/home_on.png",
+        iconPath: "../images/icons/home.png",
         pagePath: "/pages/home/home",
         text: "首页"
       },
       {
-        selectedIconPath: "../images/icons/feedback_on.png",
-        iconPath: "../images/icons/feedback.png",
+        selectedIconPath: "../images/icons/map_on.png",
+        iconPath: "../images/icons/map.png",
         pagePath: "/pages/map/map",
         text: "地图"
       },
 
       {
-        selectedIconPath: "../images/icons/feedback_on.png",
-        iconPath: "../images/icons/feedback.png",
+        selectedIconPath: "../images/icons/mine_on.png",
+        iconPath: "../images/icons/mine.png",
         pagePath: "/pages/newmine/newmine",
         text: "我的"
       }
