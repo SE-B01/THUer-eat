d407ef574eac3a7b72ea45763c3afb3180de92be
Amadeus <yuxiang-zhang2000@outlook.com>
2021-11-24 17:31:07 +0800

    getOpenId实现；稍微调整newmine界面

--- backend/user/user.py
+++ backend/user/user.py
@@ -1,5 +1,5 @@
 import json
-
+import urllib
 from flask import Blueprint, request
 from .models import User
 import sys
@@ -19,4 +19,20 @@ def index():
     res = test_dataset()
     test = str(request.args.get("text"))
     print(test)
+    return res, 200
+
+@user.route('/getUserId', methods=['GET', 'POST'])
+def getUserId():
+    appid = request.args.get("appid")
+    code = request.args.get("code")
+    secret="dedff9f3f672f0b085e33b5293ffb036"
+    url = "https://api.weixin.qq.com/sns/jscode2session"
+    url += "?appid={}".format(appid)
+    url += "&secret={}".format(secret)
+    url += "&js_code={}".format(code)
+    url += "&grant_type=authorization_code"
+    url += "&connect_redirect=1"
+    req = urllib.request.Request(url=url)
+    res = urllib.request.urlopen(req)
+    res = res.read()
     return res, 200
--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -8,9 +8,10 @@ Page({
   data: {
     TabCur: 0,
 
-    scrollLeft:0,
+    scrollLeft: 0,
     nickName: "未登录",
     is_admin: false,
+    openid: "",
 
     //宫格，不用修改
     iconList: [{
@@ -33,45 +34,121 @@ Page({
     skin: false,
 
     //选择食堂时dropDownMenu数据
-    drop_canteen_titles: ['附近', '支付方式','排序方式'],
-    drop_canteen_distance: [
-      {id: 0, title: '不限距离'},
-      {id: 1, title: '<500m'},
-      {id: 2, title: '<1km'},
-      {id: 3, title: '<3km'},
+    drop_canteen_titles: ['附近', '用餐风格', '支付方式', '排序方式'],
+    drop_canteen_distance: [{
+        id: 0,
+        title: '不限距离'
+      },
+      {
+        id: 1,
+        title: '<500m'
+      },
+      {
+        id: 2,
+        title: '<1km'
+      },
+      {
+        id: 3,
+        title: '<3km'
+      },
     ],
-    drop_canteen_payment: [
-      {id: 0, title: '支付方式不限'},
-      {id: 1, title: '仅支持校园卡'},
-      {id: 2, title: '可以使用支付宝'}
+    drop_canteen_style: [{
+        id: 0,
+        title: '风格不限'
+      },
+      {
+        id: 1,
+        title: '个人独享'
+      },
+      {
+        id: 2,
+        title: '朋友小聚'
+      },
+      {
+        id: 3,
+        title: '宴请四方'
+      }
     ],
-    drop_canteen_filter: [
-      {id: 0, title: '智能排序'},
-      {id: 1, title: '好评优先'},
-      {id: 2, title: '距离优先'}
-    ], 
+    drop_canteen_payment: [{
+        id: 0,
+        title: '支付方式不限'
+      },
+      {
+        id: 1,
+        title: '仅支持校园卡'
+      },
+      {
+        id: 2,
+        title: '可以使用支付宝'
+      }
+    ],
+    drop_canteen_filter: [{
+        id: 0,
+        title: '智能排序'
+      },
+      {
+        id: 1,
+        title: '好评优先'
+      },
+      {
+        id: 2,
+        title: '距离优先'
+      }
+    ],
 
     //选择菜品时dropDownMenu数据
-    drop_dish_titles: ['附近', '口味','用餐风格'],
-    drop_dish_distance: [
-      {id: 0, title: '不限距离'},
-      {id: 1, title: '<500m'},
-      {id: 2, title: '<1km'},
-      {id: 3, title: '<3km'},
+    drop_dish_titles: ['附近', '口味', '用餐风格'],
+    drop_dish_distance: [{
+        id: 0,
+        title: '不限距离'
+      },
+      {
+        id: 1,
+        title: '<500m'
+      },
+      {
+        id: 2,
+        title: '<1km'
+      },
+      {
+        id: 3,
+        title: '<3km'
+      },
     ],
-    drop_dish_favour: [
-      {id: 0, title: '口味不限'},
-      {id: 1, title: '清淡口味'},
-      {id: 2, title: '鲜辣口味'},
-      {id: 3, title: '大鱼大肉'},
-      {id: 4, title: '大鱼大肉'}
+    drop_dish_favour: [{
+        id: 0,
+        title: '口味不限'
+      },
+      {
+        id: 1,
+        title: '清淡口味'
+      },
+      {
+        id: 2,
+        title: '鲜辣口味'
+      },
+      {
+        id: 3,
+        title: '大鱼大肉'
+      },
     ],
-    drop_dish_filter: [
-      {id: 0, title: '风格不限'},
-      {id: 1, title: '个人独享'},
-      {id: 2, title: '朋友小聚'},
-      {id: 3, title: '宴请四方'}
-    ], 
+    drop_dish_filter: [{
+        id: 0,
+        title: '风格不限'
+      },
+      {
+        id: 1,
+        title: '个人独享'
+      },
+      {
+        id: 2,
+        title: '朋友小聚'
+      },
+      {
+        id: 3,
+        title: '宴请四方'
+      }
+    ],
 
     //餐厅信息，从数据库读取
     canteens: [],
@@ -89,97 +166,70 @@ Page({
     })
   },
   //dropDownMenu选定的选项
-  filterSelect:function(e){
+  filterSelect: function (e) {
     console.log("选中第" + e.detail.index + "个标签，选中的id：" + e.detail.selectedId + "；选中的内容：" + e.detail.selectedTitle);
   },
 
   //点击餐厅图片跳转到指定餐厅
-  switchToCanteen: function(e) {
+  switchToCanteen: function (e) {
     console.log(e)
     console.log(app.globalData)
     var canteen = e.currentTarget.dataset.canteen
     wx.navigateTo({
-      url: "../canteen/canteen?canteen="+canteen
+      url: "../canteen/canteen?canteen=" + canteen
     })
   },
   //点击菜品图片跳转到指定菜品
-  switchToDish: function(e) {
+  switchToDish: function (e) {
     console.log(e)
     console.log(app.globalData)
     var dish = e.currentTarget.dataset.dish
     console.log(dish)
     wx.navigateTo({
-      url: "../dish/dish?dish="+dish
+      url: "../dish/dish?dish=" + dish
     })
   },
 
-  onGetOpenid: function() {
+  getOpenid: function () {
+    //获得当前登录用户的openid
     var that = this
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'wechat_sign',
-      data: {
-        avatarUrl: that.data.avatarUrl,
-        gender: that.data.userInfo.gender,
-        nickName: that.data.nickName
-      },
-      success: res => {
-        console.log(res);
-        if (res.result.errCode == 0) {
-          that.setData({
-            is_admin: res.result.data.user.is_admin
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'http://127.0.0.1:5000/getUserId',
+            data: {
+              code: res.code,
+              appid: "wxf14afe0de0f6f4e7"
+              //这里没有传appsecret，放在后端了
+            },
+            success: function(res){
+              //console.log(res)
+              that.setData({
+                openid: res.data.openid
+              })
+              app.globalData.openid = that.data.openid
+            }
           })
-          app.globalData.logged = true
-          app.globalData.is_admin = res.result.data.user.is_admin
-          that.getTabBar().changeFormat()
-          that.data.user = res.result.data.user
-          app.globalData.user = res.result.data.user
         } else {
-          wx.showModal({
-            title: '抱歉，出错了呢~',
-            content: res.result.errMsg,
-            confirmText: "我知道了",
-            showCancel: false,
-            success(res) {
-              if (res.confirm) {
-                console.log('用户点击确定')
-              } else if (res.cancel) {
-                console.log('用户点击取消')
-              }
-            }
-          })
+          console.log('获取用户登录态失败！' + res.errMsg)
         }
-      },
-      fail: err => {
-        console.error('[云函数] [wechat_sign] 调用失败', err)
-        wx.showModal({
-          title: '调用失败',
-          content: '请检查云函数是否已部署',
-          showCancel: false,
-          success(res) {
-            if (res.confirm) {
-              console.log('用户点击确定')
-            } else if (res.cancel) {
-              console.log('用户点击取消')
-            }
-          }
-        })
       }
-    })
+    });
   },
 
   getSelectCanteens: function () {
     var that = this
     wx.request({
       url: 'http://127.0.0.1:5000/get_select_canteens',
-      data: {
-      },
+      data: {},
       method: 'GET',
       success: (res) => {
         console.log("get canteens success")
         //console.log(res.data)
         that.setData({
-          canteens:res.data,
+          canteens: res.data,
         })
       }
     })
@@ -189,14 +239,13 @@ Page({
     var that = this
     wx.request({
       url: 'http://127.0.0.1:5000/get_select_dishes',
-      data: {
-      },
+      data: {},
       method: 'GET',
       success: (res) => {
         console.log("get dishes success")
         //console.log(res.data)
         that.setData({
-          dishes:res.data,
+          dishes: res.data,
         })
       }
     })
@@ -205,12 +254,14 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  
-  onLoad: function(options) {
+
+  onLoad: function (options) {
     var that = this
     //加载页面函数
     that.getSelectCanteens()
     that.getSelectDishes()
+    //测试：getOpenid
+    that.getOpenid()
     //登录模块
     if (!wx.cloud) {
       wx.showModal({
@@ -247,9 +298,9 @@ Page({
                   userInfo: res.userInfo
                 })
                 app.globalData.userInfo = res.userInfo
-                console.log('thererererer')
+                console.log('登陆成功')
                 console.log(that.data.userInfo)
-                
+
               }
             })
 
@@ -258,7 +309,7 @@ Page({
           }
         }
       })
-    } else{
+    } else {
       return
     }
 
@@ -267,7 +318,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
@@ -275,13 +326,13 @@ Page({
    */
   onShow: function () {
     if (typeof this.getTabBar === 'function' &&
-    this.getTabBar()) {
-    this.getTabBar().setData({
-      selected: 0
-    })
-    this.getTabBar().changeFormat()
-  }
-    
+      this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0
+      })
+      this.getTabBar().changeFormat()
+    }
+
 
   },
 
