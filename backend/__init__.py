c88e4b0039c3427368ca91b8d078e3db08e248a0
Amadeus <yuxiang-zhang2000@outlook.com>
2021-12-04 16:08:19 +0800

    search页面完成餐厅部分

--- backend/__init__.py
+++ backend/__init__.py
@@ -8,7 +8,7 @@ def create_app(config_name='default'):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     #config[config_name].init_app(app)
     from . import dish, canteen, appraise, collection, \
-        recommend, feedback, user, recent_view, appraise_dish_mapping, db
+        recommend, feedback, user, recent_view, db
     db.init_app(app)
     dish.init_app(app)
     canteen.init_app(app)
@@ -17,7 +17,6 @@ def create_app(config_name='default'):
     user.init_app(app)
     feedback.init_app(app)
     recent_view.init_app(app)
-    #test_image.init_app(app)
     recommend.init_app(app)
     appraise_dish_mapping.init_app(app)
     return app
