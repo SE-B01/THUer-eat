0165309eef3ae5aff70e0e9d7fa002df925667e4
Amadeus <yuxiang-zhang2000@outlook.com>
2021-12-17 00:50:32 +0800

    评论详情页面

---
+++ miniprogram/pages/appraise/appraise.js
@@ -0,0 +1,69 @@
+// pages/appraise/appraise.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    user_avatar: "../../images/dishes/唐可可.webp",
+    user_name: "唐可可",
+    comment: "哈哈哈哈哈",
+    img_list: ["../../images/dishes/葱油饼.jfif", "../../images/dishes/打卤面.jfif", "../../images/dishes/石锅拌饭.jfif", "../../images/dishes/铁板鸡饭.jfif"]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
