fc810970937269ac4dc72c996f6c2d9236cf5613
ylf <2872444509@qq.com>
2021-12-21 21:12:42 +0800

    fix frontend bugs

--- miniprogram/pages/appraise/appraise.js
+++ miniprogram/pages/appraise/appraise.js
@@ -44,7 +44,7 @@ Page({
       mask: true
     })
     wx.request({
-      url: 'http://127.0.0.1:5000/appraise/changeLiked',
+      url: 'http://'+app.globalData.IpAddress + '/appraise/changeLiked',
       data: {
         user_id: app.globalData.openid,
         like_changed: this.data.appraise_id
@@ -68,7 +68,7 @@ Page({
     var appraise_id = appraise_id
     var that = this
     wx.request({
-      url: 'http://127.0.0.1:5000/appraise/get_detail',
+      url: 'http://'+app.globalData.IpAddress + '/appraise/get_detail',
       data: {
         user_id: id,
         appraise_id: appraise_id,
